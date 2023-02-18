aa800b02742b4bee25060d7ef063c6640bfd24bb
Jairo Vidomines <jvidomines@gmail.com>
2023-02-18 10:58:49 +0100

    Create variable environment

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,11 @@ import "./loadenvironment.js";
 import express from "express";
 
 const app = express();
+
+const port = process.env.PORT ?? 4001;
+
+app.get("/", (req, res) => {
+  res.json({ pong: true });
+});
+
+app.listen(port);
