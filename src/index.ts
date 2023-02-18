c124f16a3e7ec87c0a40621afa803da8273d781f
Jairo Vidomines <jvidomines@gmail.com>
2023-02-18 15:33:58 +0100

    Create router

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT ?? 4001;
 
 app.get("/robots", (req, res) => {
-  res.status(200).json({ pong: true });
+  res.status(201).json({ pong: true });
 });
 
 app.listen(port);
