a0557839123552f9aa6cabc11d5624824f1b73f5
Diana Vidal <dvidaal@gmail.com>
2023-02-20 21:00:11 +0100

    Create user controller for login and structure user

--- src/server/routers/router.ts
+++ src/server/routers/router.ts
@@ -1,8 +1,13 @@
 import Router from "express";
 import { getRobots, getRobotById } from "../controllers/robotsControllers.js";
+import { loginUser } from "../controllers/userControllers.js";
 
 // eslint-disable-next-line new-cap
 export const robotsRouter = Router();
+// eslint-disable-next-line new-cap
+export const usersRouter = Router();
 
 robotsRouter.get("/", getRobots);
 robotsRouter.get("/:idRobot", getRobotById);
+
+usersRouter.get("/", loginUser);
